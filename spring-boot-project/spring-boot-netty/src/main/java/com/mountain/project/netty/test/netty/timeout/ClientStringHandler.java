6a17ceb8d1071af29e0374b44c3fed7f281d11ed
kejiefu <kejiefu@126.com>
2021-01-26 13:54:41 +0800

    客户端退出通知事件

--- spring-boot-project/spring-boot-netty/src/main/java/com/mountain/project/netty/test/netty/timeout/ClientStringHandler.java
+++ spring-boot-project/spring-boot-netty/src/main/java/com/mountain/project/netty/test/netty/timeout/ClientStringHandler.java
@@ -13,6 +13,7 @@ public class ClientStringHandler extends ChannelInboundHandlerAdapter {
 
     /**
      * 这条连接上添加的所有的业务逻辑处理器都被移除掉后调用
+     *
      * @param ctx
      */
     @Override
@@ -20,11 +21,29 @@ public class ClientStringHandler extends ChannelInboundHandlerAdapter {
         System.out.println("handlerRemoved..." + System.currentTimeMillis());
     }
 
+    /**
+     * 当出现异常就关闭连接
+     * 因于Netty的I/O异常或一个处理器实现的内部异常。多数情况下，捕捉到的异常应当被记录下来，
+     * 并在这个方法中关闭这个channel通道。当然处理这种异常情况的方法实现可能因你的实际需求而有所不同，例如，在关闭这个连接之前你可能会发送一个包含了错误码的响应消息。
+     *
+     * @param ctx
+     * @param cause
+     */
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        System.out.println("exceptionCaught....");
+        System.out.println("exceptionCaught...." + System.currentTimeMillis());
         cause.printStackTrace();
         ctx.close();
+    }
+
+    /**
+     * 客户端退出通知事件（channelInactive）
+     *
+     * @param ctx
+     */
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) {
+        System.out.println("channelInactive..." + System.currentTimeMillis());
     }
 
     public static void main(String[] args) {
